d0f430e1caa489e21c1e103848279869f2c409a2
Benice Hattingh <benicehietbrink@gmail.com>
2021-11-19 15:52:55 +0200

    Added post and delete

--- WebApplicationToDo/Controllers/Controller1.cs
+++ WebApplicationToDo/Controllers/Controller1.cs
@@ -8,28 +8,47 @@ using WebApplicationToDo.Models;
 
 namespace WebApplicationToDo.Controllers
 {
-    [Route("/todos")]
+    [Route("todos")]
     public class TestController : ApiController
     {
         public static List<Model1> lTodo = new List<Model1>()
         {
-            new Model1{id = 0, name = "Do some Work", completed = false}
+            new Model1{id = 0, name = "Do some work", completed = false}
         };
 
         [HttpGet]
         public List<Model1> Get()
         {
+            return lTodo;
+        }
 
+        [HttpPost]
+        public List<Model1> Post([FromBody] Model1 model)
+        {
+            lTodo.Add(model);
             return lTodo;
         }
 
         [HttpPut]
         [Route("{id}")]
-        public Model1 Put(int id)//update todo
+        public List<Model1> Put(int id, [FromBody] Model1 model)//update todo
         {
-            Model1 Todo = lTodo.Find(i => i.id == id);
-            Todo.completed = true;
-            return Todo;
+            Model1 TodoUpdate = lTodo.Find(i => i.id == id);
+            int index = lTodo.IndexOf(TodoUpdate);
+            lTodo[index].completed = true;
+            lTodo[index].name = "Done some work";
+
+            return lTodo;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public List<Model1> Delete(int id)//delete todo
+        {
+            Model1 Tododel = lTodo.Find(i => i.id == id);
+            lTodo.Remove(Tododel);
+
+            return lTodo;
         }
     }
 }

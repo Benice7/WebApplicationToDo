1b0b1b42da743d960968e93eb6565774ba0c3b67
Benice Hattingh <benicehietbrink@gmail.com>
2021-11-19 13:35:45 +0200

    Added controller and Model to get started

---
+++ WebApplicationToDo/Models/Model1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationToDo.Models
+{
+    public class Model1
+    {
+
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool completed { get; set; }
+
+    }
+}
